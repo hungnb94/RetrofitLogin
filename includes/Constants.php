cf1e300bb20d6dec73fb4a17146d9640c42d06cd
Hung Nguyen <hungnb94@gmail.com>
2017-09-18 17:43:38 +0700

    first commit abc

---
+++ includes/Constants.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hung
+ * Date: 9/17/2017
+ * Time: 8:35 AM
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'retrofit_login');
+
+define('USER_CREATED', 101);
+define('USER_EXIST', 102);
+define('USER_CREATION_FAILED', 103);
---
+++ includes/DbConnect.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hung
+ * Date: 9/17/2017
+ * Time: 8:36 AM
+ */
+
+class DbConnect
+{
+    //Variable to store database link
+    private $con;
+
+    //Class constructor
+    function __construct()
+    {
+
+    }
+
+    //This method will connect to the database
+    function connect()
+    {
+        //Including the constants.php file to get the database constants
+        include_once dirname(__FILE__) . '/Constants.php';
+
+        //connecting to mysql database
+        $this->con = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+        //Checking if any error occured while connecting
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            return null;
+        }
+
+        //finally returning the connection link
+        return $this->con;
+    }
+}
